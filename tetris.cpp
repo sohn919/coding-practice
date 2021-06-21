db8048553c9c5d5f49a02bfd331d50b915720e14
sohn919 <sohn919@naver.com>
2021-06-21 18:24:23 +0900

    백 그라운드

--- tetris.cpp
+++ tetris.cpp
@@ -1,38 +1,15 @@
 #include <stdio.h> 
-#include <stdlib.h>
-#include <time.h>
-#include <windows.h>
+
+void background()
+{
+	char background[100] = {
+		'm','m','m','m','m','m','m','m','m';
+	
+	}
+}
 
 int main(void)
 {
-	int a;
-	int val;
-	srand(time(NULL));
 	
-	a = (rand() % 3) + 1;
-	
-	
-	switch(a) {
-		while(1) {
-		val = (rand() % 3) + 1;
-		printf("%d", val);
-		case 1:
-			if(val == 1) {
-			printf("hi");
-			break;
-		}
-		case 2:
-			if(val == 1) {
-			printf("hi");
-			break;
-		}
-		case 3:
-			if(val == 1) {
-			printf("hi");
-			break;
-		}
-	}
-	Sleep(1000);
-}
 	
 }
