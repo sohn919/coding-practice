056f2058d0f00a65fc60d121c886caa0649fe885
sohn919 <sohn919@naver.com>
2021-07-29 23:11:51 +0900

    2

--- C/2.c
+++ C/2.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 
-void arr_set(char a[], int n, char val)
+int main()
 {
-	int i;
+	int i,j;
+	int a;
+	int count[10] = {0};
+	int max = 0;
 	
-	for(i=0; i<n; i++) {
-		a[i] = val;
+	while((a = getchar()) != EOF) {
+		if(a >= '0' && a <= '9') {
+			count[a-'0']++;
+		}
 	}
-}
-
-int main() {
-	int i;
-	char *animal[5] = {
-		"dog", "cat", "rabbit", "panda", "bear"
-	};
-	
-	arr_set(&animal[1], 2, "h");
-	
-	for(i=0; i<5; i++) {
-		printf("animal[%d] = %s\n", i, animal[i]);
+	for(i=0; i<10; i++) {
+		if(count[i] > max) {
+			max = count[i];
+		}
 	}
 	
+	printf("숫자가 등장한 횟수\n");
+	for(i=max; i>=1; i--) {
+		for(j=0; j<10; j++) {
+			if(count[j] >= i) {
+				printf(" * ");
+			} else {
+				printf("   ");
+			}
+			printf("\n");
+		}
+	}
+	
+	for(j=0; j<10; j++) {
+		printf(" %d", j);
+	}
 }
